e85ca782ae8a0a5a10d14f0e9ff5c2936037ce66
Rus Chr <r.chergizbiev@gmail.com>
2022-11-30 14:49:54 +0300

    Initial Commit

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Rustam Chergizbiev on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
