7c2dcb9ba21bfa593401cbd696c89c4aab3a5a4b
Rus Chr <r.chergizbiev@gmail.com>
2022-11-30 15:03:26 +0300

    test 2

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world! 234")
+            Text("Hello, world! 234 wer")
         }
         .padding()
     }
