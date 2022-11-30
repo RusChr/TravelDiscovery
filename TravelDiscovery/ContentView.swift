5a9eb88651c6a044a47b9819b3ad9fa645f5ff57
Rus Chr <r.chergizbiev@gmail.com>
2022-11-30 17:02:36 +0300

    lesson 1: done

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -8,19 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world! 234 wer")
-        }
-        .padding()
-    }
+	var body: some View {
+		NavigationView {
+			ScrollView {
+				
+				DiscoverCategoryView()
+				
+			}
+			.navigationTitle("Discover")
+		}
+	}
 }
 
+
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
+}
+
+struct DiscoverCategoryView: View {
+	var body: some View {
+		ScrollView(.horizontal, showsIndicators: false) {
+			HStack(spacing: 8) {
+				ForEach(0..<5, id: \.self) { _ in
+					VStack(spacing: 8) {
+						Spacer()
+							.frame(width: 50, height: 50)
+							.background(Color.gray)
+							.cornerRadius(.infinity)
+							.shadow(color: Color(.lightGray), radius: 2, x: 0, y: 2)
+						Text("Art")
+							.font(.system(size: 14, weight: .semibold))
+					}
+				}
+			}.padding(.horizontal)
+		}
+	}
 }
